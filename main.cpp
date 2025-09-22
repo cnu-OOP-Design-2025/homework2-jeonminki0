06cdf16aed2849733c41d0c85ddc044b674ba4b4
jeonminki0 <bean4851@naver.com>
2025-09-22 20:27:02 +0900

    과제 제출

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "homework2-2.h"
 using namespace std;
 
-StudentStruct *students = nullptr;
+StudentStruct* students = nullptr;
 int numOfStudent = 0;
 
 void printBestStudent()
@@ -21,53 +21,52 @@ void printBestStudent()
     cout << "Best Total Score Student:\n";
     printStudentInfo(students, numOfStudent, bestTotalId);
 }
-void printAverage()
-{
+
+void printAverage() {
     float midtermAvg = getMidtermAverage(students, numOfStudent);
     float finalAvg = getFinalAverage(students, numOfStudent);
     float totalAvg = getTotalAverage(students, numOfStudent);
 
     cout << "# of Student: " << numOfStudent << endl;
     cout << "Midterm Average: " << midtermAvg << endl;
-    cout << "Midterm Average: " << finalAvg << endl;;
-    cout << "Midterm Average: " << totalAvg << endl;;
+    cout << "Midterm Average: " << finalAvg << endl;
+    cout << "Midterm Average: " << totalAvg << endl;
 }
 
-void printStudentList(){
-    for(int id = 1000; id < 1100; ++id){
+void printStudentList() {
+    for (int id = 1000; id < 1100; ++id) {
         printStudentInfo(students, numOfStudent, id);
     }
 }
 
-void doNamespaceTest(){
+void doNamespaceTest() {
     cout << "---------------------------" << endl;
     cout << "Namespace Test" << endl;
 
     // 정수형 계산기 테스트
-    cout << "[IntCalc] 10 + 3 = " << 0 /*TODO*/ << endl;
-    cout << "[IntCalc] 10 - 3 = " << 0 /*TODO*/ << endl;
-    cout << "[IntCalc] 10 * 3 = " << 0 /*TODO*/ << endl;
-    cout << "[IntCalc] 10 / 3 = " << 0 /*TODO*/ << endl;
+    cout << "[IntCalc] 10 + 3 = " << IntCalc::add(10, 3) << endl;
+    cout << "[IntCalc] 10 - 3 = " << IntCalc::subtract(10, 3) << endl;
+    cout << "[IntCalc] 10 * 3 = " << IntCalc::multiply(10, 3) << endl;
+    cout << "[IntCalc] 10 / 3 = " << IntCalc::divide(10, 3) << endl;
 
-    cout << "[IntCalc] 10.5 + 3.2 = " << 0 /*TODO*/ << endl;
-    cout << "[IntCalc] 10.5 - 3.2 = " << 0 /*TODO*/ << endl;
-    cout << "[IntCalc] 10.5 * 3.2 = " << 0 /*TODO*/ << endl;
-    cout << "[IntCalc] 10.5 / 3.2 = " << 0 /*TODO*/ << endl;
-
+    cout << "[IntCalc] 10.5 + 3.2 = " << IntCalc::add(10.5f, 3.2f) << endl;
+    cout << "[IntCalc] 10.5 - 3.2 = " << IntCalc::subtract(10.5f, 3.2f) << endl;
+    cout << "[IntCalc] 10.5 * 3.2 = " << IntCalc::multiply(10.5f, 3.2f) << endl;
+    cout << "[IntCalc] 10.5 / 3.2 = " << IntCalc::divide(10.5f, 3.2f) << endl;
 
     // 실수형 계산기 테스트
-    cout << "[FloatCalc] 10 + 3 = " << 0.0f /*TODO*/ << endl;
-    cout << "[FloatCalc] 10 - 3 = " << 0.0f /*TODO*/ << endl;
-    cout << "[FloatCalc] 10 * 3 = " << 0.0f /*TODO*/ << endl;
-    cout << "[FloatCalc] 10 / 3 = " << 0.0f /*TODO*/ << endl;
+    cout << "[FloatCalc] 10 + 3 = " << FloatCalc::add(10, 3) << endl;
+    cout << "[FloatCalc] 10 - 3 = " << FloatCalc::subtract(10, 3) << endl;
+    cout << "[FloatCalc] 10 * 3 = " << FloatCalc::multiply(10, 3) << endl;
+    cout << "[FloatCalc] 10 / 3 = " << FloatCalc::divide(10, 3) << endl;
 
-
-    cout << "[FloatCalc] 10.5 + 3.2 = " << 0.0f /*TODO*/ << endl;
-    cout << "[FloatCalc] 10.5 - 3.2 = " << 0.0f /*TODO*/ << endl;
-    cout << "[FloatCalc] 10.5 * 3.2 = " << 0.0f /*TODO*/ << endl;
-    cout << "[FloatCalc] 10.5 / 3.2 = " << 0.0f /*TODO*/ << endl;
+    cout << "[FloatCalc] 10.5 + 3.2 = " << FloatCalc::add(10.5f, 3.2f) << endl;
+    cout << "[FloatCalc] 10.5 - 3.2 = " << FloatCalc::subtract(10.5f, 3.2f) << endl;
+    cout << "[FloatCalc] 10.5 * 3.2 = " << FloatCalc::multiply(10.5f, 3.2f) << endl;
+    cout << "[FloatCalc] 10.5 / 3.2 = " << FloatCalc::divide(10.5f, 3.2f) << endl;
 }
-void doTest1(){
+
+void doTest1() {
     cout << "---------------------------" << endl;
     cout << "Test 1" << endl;
 
@@ -75,56 +74,47 @@ void doTest1(){
     printAverage();
     printStudentList();
 }
-void doTest2(){
+
+void doTest2() {
     cout << "---------------------------" << endl;
     cout << "Test 2" << endl;
 
     StudentStruct charlie("Charlie", 1003, 70, 99.0);
 
-
-    int idx = -1;
-    /* TODO: homework1.cpp의 findStudentByStudentID() 함수 호출을 homework2-2.h의 findStudentByStudentID 함수 선언에 맞춰 수정하기 */
-    /*
-    idx = findStudentByStudentID(charlie.id);
-    */
-
-    if(idx >= 0)
+    int idx = findStudentByStudentID(students, numOfStudent, charlie.id);
+    if (idx >= 0)
         modifyRecord(students, numOfStudent, charlie);
 
-    /* TODO: homework1.cpp의 addStudent() 함수 호출를 homework2-2.h의 addStudent 함수 선언에 맞춰 수정하기 */
-    /*
-    addStudent("Ana", 1051, 88, 65);
-    addStudent("Suji", 1052, 90, 93);
-    addStudent("Zhang", 1053, 100, 40);
-    */
+    addStudent(students, &numOfStudent, "Ana", 1051, 88, 65);
+    addStudent(students, &numOfStudent, "Suji", 1052, 90, 93);
+    addStudent(students, &numOfStudent, "Zhang", 1053, 100, 40);
 
     printBestStudent();
     printAverage();
     printStudentList();
 }
-void doTest3(){
+
+void doTest3() {
     std::cout << "---------------------------" << std::endl;
     std::cout << "Test 3" << std::endl;
 
-    /* TODO: homework1.cpp의 addStudent 함수를 homework2-2.h의 addStudent 함수에 맞춰 수정하기 */
-    /*
-    deleteStudent(1011);
-    deleteStudent(1029);
-    */
+    deleteStudent(students, &numOfStudent, 1011);
+    deleteStudent(students, &numOfStudent, 1029);
 
     printStudentList();
 }
 
 void doPointerTest(int select){
     const int MAX_STUDENTS = 100;
-    /* TODO: StudentStruct[MAX_STUDENTS] 메모리 동적 할당 */
-    students = nullptr;
+    students = new StudentStruct[MAX_STUDENTS];
+    numOfStudent = 0;
+
     fillStudentRecord(students, &numOfStudent);
 
-    if(students == nullptr)
+    if (students == nullptr)
         return;
 
-    switch(select){
+    switch (select) {
         case 2:
             doTest1();
             break;
@@ -140,13 +130,14 @@ void doPointerTest(int select){
             doTest3();
             break;
     }
+
     delete[] students;
 }
-int main(int argc, char **argv) {
 
-    if(argc == 2){
+int main(int argc, char** argv) {
+    if (argc == 2) {
         int select = atoi(argv[1]);
-        switch(select){
+        switch (select) {
             case 1:
                 doNamespaceTest();
                 return 0;
@@ -157,8 +148,8 @@ int main(int argc, char **argv) {
                 return 0;
         }
     }
+
     doNamespaceTest();
     doPointerTest(0);
-
     return 0;
 }
